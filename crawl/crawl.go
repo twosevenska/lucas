669e0509509149f4579163db10f184b45266994c
goncalo.barroso <goncalo.barroso@headforwards.com>
2019-03-29 16:20:49 +0000

    WIP: Split workload

--- crawl/crawl.go
+++ crawl/crawl.go
@@ -17,6 +17,8 @@ func Download(address string) (string, error) {
 
 	// Warning: This will not handle large webpages properly
 	// https://groups.google.com/forum/#!topic/golang-nuts/sAwDldpkMGQ
+	// It might be better to temporarily download to a file and scan it
+	// line by line
 	html, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", fmt.Errorf("failed to read webpage: %s", err.Error())
@@ -47,13 +49,35 @@ func extractURL(line string) (string, bool) {
 	return result, true
 }
 
+// ExtractURLS finds and extracts urls in page from hyperlink tags
 func ExtractURLS(page string) map[string]int {
+	lines := make(chan string)
+	defer close(lines)
+
+	done := make(chan bool)
+	defer close(done)
+
 	addresses := make(map[string]int)
-	for _, l := range strings.Split(page, "\n") {
-		u, extracted := extractURL(l)
-		if extracted {
-			addresses[u] = addresses[u] + 1
+	go func() {
+		for {
+			select {
+			case l := <-lines:
+				u, extracted := extractURL(l)
+				if extracted {
+					addresses[u] = addresses[u] + 1
+				}
+			case d := <-done:
+				if d {
+					return
+				}
+			}
 		}
+	}()
+
+	for _, l := range strings.Split(page, "\n") {
+		lines <- l
 	}
+	done <- true
+
 	return addresses
 }
